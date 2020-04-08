51a758d8a0952627aac3916f6ae88bfa4e1e117c
linlinge <superlinlinge@163.com>
2020-04-08 22:03:46 +0800

    update

---
+++ build/CMakeFiles/lab_pcl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_pcl.dir/main.cpp.o"
+  "lab_pcl.pdb"
+  "lab_pcl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_pcl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <pcl/point_types.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/surface/gp3.h>
+#include <pcl/octree/octree.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/centroid.h>
+#include <pcl/features/boundary.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <omp.h>
+#include <vector>
+using namespace std;
+int main()
+{
+    return 0;
+}
